cc46dcdafcbb2976be3df0a267f676d9b80b66a2
Ikhwanul Labib <ikhwanul.labib@gmail.com>
2024-05-01 09:02:11 +0700

    add Invalid Affix Pair Specification Test

---
+++ Sastrawi.Tests/UnitTests/Morphology/InvalidAffixPairSpecificationTest.cs
@@ -0,0 +1,22 @@
+using Morphology;
+
+namespace Sastrawi.Tests.UnitTests.Morphology;
+
+public class DisambiguatorPrefixRule1aTest
+{
+    [Fact]
+    public void TestContainsInvalidAffixPair()
+    {
+        var specification = new InvalidAffixPairSpecification();
+        Assert.False(specification.IsSatisfiedBy("memberikan"));
+        Assert.False(specification.IsSatisfiedBy("ketahui"));
+
+        Assert.True(specification.IsSatisfiedBy("berjatuhi"));
+        Assert.True(specification.IsSatisfiedBy("dipukulan"));
+        Assert.True(specification.IsSatisfiedBy("ketiduri"));
+        Assert.True(specification.IsSatisfiedBy("ketidurkan"));
+        Assert.True(specification.IsSatisfiedBy("menduaan"));
+        Assert.True(specification.IsSatisfiedBy("terduaan"));
+        Assert.True(specification.IsSatisfiedBy("perkataan"));
+    }
+}
